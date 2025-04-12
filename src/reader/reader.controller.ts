75d9161cc5e86b5466b364aa1dd1961aa965eb21
rafaelcaet <fael.caetsantos@gmail.com>
2025-04-12 08:55:27 -0300

    feat: add upload-pdf endpoint

--- src/reader/reader.controller.ts
+++ src/reader/reader.controller.ts
@@ -10,10 +10,19 @@ import { ReaderService } from "./reader.service";
 @Controller("reader")
 export class ReaderController {
   constructor(private readonly readerService: ReaderService) {}
-  // Rota publica
-  @Post("upload")
+  // Rota publica para upload do csv
+  @Post("upload-csv")
   @UseInterceptors(FileInterceptor("file")) // intercepta a request pegando o `file`
+  // tipagem do file com o Multer
   async uploadCSV(@UploadedFile() file: Express.Multer.File) {
     return this.readerService.parserCSVFile(file);
+  }
+
+  // Rota publica para upload do pdf
+  @Post("upload-pdf")
+  @UseInterceptors(FileInterceptor("file")) // intercepta a request pegando o `file`
+  // tipagem do file com o Multer
+  async uploadPDF(@UploadedFile() file: Express.Multer.File) {
+    return this.readerService.parserPDFFile(file);
   }
 }
--- src/reader/reader.service.ts
+++ src/reader/reader.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma/prisma.service";
 import { parse } from "csv-parse/sync";
+import { PDFDocument } from "pdf-lib";
+import { writeFile } from "fs/promises";
+import { mkdirSync, existsSync } from "fs";
+import { join } from "path";
+import * as os from "os";
 
 @Injectable()
 export class ReaderService {
   constructor(private prisma: PrismaService) {}
 
-  // tipagem e upload do arquivo usando o express.multer
+  // Parser para o arquivo CSV
   async parserCSVFile(file: Express.Multer.File) {
     try {
       const csvContent = file.buffer.toString("utf-8");
-      // coleta as informacoes do csv (parser)
+      // Coleta as informações do CSV (parser)
       const results = parse(csvContent, {
         delimiter: ";",
         columns: true,
         skip_empty_lines: true,
         trim: true,
       });
-      // formata as informacoes, garantindo que busque no banco de `lots` pelo nome do lote contido no boleto
+
+      // Formata as informações, buscando no banco de `lots` pelo nome do lote contido no boleto
       const formattedData = await Promise.all(
         results.map(async (line: any) => {
           const lot = await this.prisma.lots.findFirst({
@@ -31,9 +37,10 @@ export class ReaderService {
           });
 
           if (!lot) {
-            throw new Error(`Lot not found ${line.unidade}`);
+            throw new Error(`Lote não encontrado para unidade ${line.unidade}`);
           }
-          // retorna o obj formatado para insercao on banco
+
+          // Retorna o objeto formatado para inserção no banco
           return {
             withdrawName: line.nome,
             lotId: lot.id,
@@ -45,15 +52,53 @@ export class ReaderService {
           };
         })
       );
-      // insercao em `batch` no banco
+
+      // Inserção em batch no banco
       const created = await this.prisma.billets.createMany({
         data: formattedData,
         skipDuplicates: true,
       });
-      // retorna a qnt de objetos inseridos
+
+      // Retorna a quantidade de objetos inseridos
       return { inserted: created.count };
     } catch (err) {
       throw err;
     }
+  }
+
+  // Função para processar o PDF
+  async parserPDFFile(file: Express.Multer.File) {
+    const pdfFile = await PDFDocument.load(file.buffer);
+    const totalPages = pdfFile.getPageCount();
+
+    // fetch dos boletos (nome e id)
+    const billets = await this.prisma.billets.findMany({
+      orderBy: { id: "asc" }, // garante que a ordem esteja conforme desejado
+      select: { id: true, withdrawName: true },
+    });
+
+    // path para criar a pasta local
+    const desktopPath =
+      os.platform() === "win32"
+        ? join(os.homedir(), "Desktop", "billets")
+        : join(os.homedir(), "Desktop", "billets");
+
+    // garante que a pasta existe (cria se nao existir)
+    if (!existsSync(desktopPath)) {
+      mkdirSync(desktopPath, { recursive: true });
+    }
+
+    // le o pdf e cria os arquivos com o respectivo id na tabela `Billets`
+    for (let i = 0; i < totalPages; i++) {
+      const newPdf = await PDFDocument.create();
+      const [copiedPage] = await newPdf.copyPages(pdfFile, [i]);
+      newPdf.addPage(copiedPage);
+
+      const pdfBytes = await newPdf.save();
+      const filePath = join(desktopPath, `${billets[i].id}.pdf`);
+      await writeFile(filePath, pdfBytes);
+    }
+
+    return { message: "All files were generated successfully" };
   }
 }
